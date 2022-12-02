045db9b9c1fae8c8e6234c55aff46cfee609fbca
akif.uluca <akif.uluca@vodafone.com>
2022-12-02 10:32:44 +0300

    Commiting for pull.

--- src/test/java/com/project/runners/Runner.java
+++ src/test/java/com/project/runners/Runner.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 @CucumberOptions(
         features = "src/test/features/",
-        tags = "@Test",
+        tags = "@Test953024",
         plugin = {"pretty", "io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm",
                 "json:target/cucumber-reports/Cucumber.json",
                 "junit:target/cucumber-reports/Cucumber.xml",
